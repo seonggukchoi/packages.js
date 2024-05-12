21c101ef06ce82c09b34577b5fefb76850d446aa
seonggukchoi <seonggukchoi.kr@gmail.com>
2024-05-12 16:36:51 +0000

    fix(datee): replace all characters in template string

--- packages/datee/src/datee.ts
+++ packages/datee/src/datee.ts
@@ -37,8 +37,8 @@ export class Datee extends Date {
   private formatYear(format: string): string {
     let formatted = format;
 
-    formatted = formatted.replace('YYYY', this.getFullYear().toString());
-    formatted = formatted.replace('YY', this.getFullYear().toString().slice(-2));
+    formatted = formatted.replaceAll('YYYY', this.getFullYear().toString());
+    formatted = formatted.replaceAll('YY', this.getFullYear().toString().slice(-2));
 
     return formatted;
   }
@@ -46,10 +46,10 @@ export class Datee extends Date {
   private formatMonth(format: string): string {
     let formatted = format;
 
-    formatted = formatted.replace('MMMM', this.toLocaleString('en', { month: 'long' }));
-    formatted = formatted.replace('MMM', this.toLocaleString('en', { month: 'short' }));
-    formatted = formatted.replace('MM', (this.getMonth() + 1).toString().padStart(2, '0'));
-    formatted = formatted.replace('M', (this.getMonth() + 1).toString());
+    formatted = formatted.replaceAll('MMMM', this.toLocaleString('en', { month: 'long' }));
+    formatted = formatted.replaceAll('MMM', this.toLocaleString('en', { month: 'short' }));
+    formatted = formatted.replaceAll('MM', (this.getMonth() + 1).toString().padStart(2, '0'));
+    formatted = formatted.replaceAll('M', (this.getMonth() + 1).toString());
 
     return formatted;
   }
@@ -57,8 +57,8 @@ export class Datee extends Date {
   private formatDate(format: string): string {
     let formatted = format;
 
-    formatted = formatted.replace('DD', this.getDate().toString().padStart(2, '0'));
-    formatted = formatted.replace('D', this.getDate().toString());
+    formatted = formatted.replaceAll('DD', this.getDate().toString().padStart(2, '0'));
+    formatted = formatted.replaceAll('D', this.getDate().toString());
 
     return formatted;
   }
@@ -66,7 +66,7 @@ export class Datee extends Date {
   private formatQuarter(format: string): string {
     let formatted = format;
 
-    formatted = formatted.replace('Q', Math.floor(this.getMonth() / 3).toString());
+    formatted = formatted.replaceAll('Q', Math.floor(this.getMonth() / 3).toString());
 
     return formatted;
   }
@@ -74,9 +74,9 @@ export class Datee extends Date {
   private formatDay(format: string): string {
     let formatted = format;
 
-    formatted = formatted.replace('WWW', this.toLocaleString('en', { weekday: 'long' }));
-    formatted = formatted.replace('WW', this.toLocaleString('en', { weekday: 'short' }));
-    formatted = formatted.replace('W', this.getDay().toString());
+    formatted = formatted.replaceAll('WWW', this.toLocaleString('en', { weekday: 'long' }));
+    formatted = formatted.replaceAll('WW', this.toLocaleString('en', { weekday: 'short' }));
+    formatted = formatted.replaceAll('W', this.getDay().toString());
 
     return formatted;
   }
@@ -84,8 +84,8 @@ export class Datee extends Date {
   private formatWeek(format: string): string {
     let formatted = format;
 
-    formatted = formatted.replace('ww', this.getWeek().toString().padStart(2, '0'));
-    formatted = formatted.replace('w', this.getWeek().toString());
+    formatted = formatted.replaceAll('ww', this.getWeek().toString().padStart(2, '0'));
+    formatted = formatted.replaceAll('w', this.getWeek().toString());
 
     return formatted;
   }
@@ -93,10 +93,10 @@ export class Datee extends Date {
   private formatHour(format: string): string {
     let formatted = format;
 
-    formatted = formatted.replace('HH', this.getHours().toString().padStart(2, '0'));
-    formatted = formatted.replace('H', this.getHours().toString());
-    // formatted = formatted.replace('hh', (this.getHours() % 12 || 12).toString().padStart(2, '0'));
-    // formatted = formatted.replace('h', (this.getHours() % 12 || 12).toString());
+    formatted = formatted.replaceAll('HH', this.getHours().toString().padStart(2, '0'));
+    formatted = formatted.replaceAll('H', this.getHours().toString());
+    // formatted = formatted.replaceAll('hh', (this.getHours() % 12 || 12).toString().padStart(2, '0'));
+    // formatted = formatted.replaceAll('h', (this.getHours() % 12 || 12).toString());
 
     return formatted;
   }
@@ -104,8 +104,8 @@ export class Datee extends Date {
   private formatMinute(format: string): string {
     let formatted = format;
 
-    formatted = formatted.replace('mm', this.getMinutes().toString().padStart(2, '0'));
-    formatted = formatted.replace('m', this.getMinutes().toString());
+    formatted = formatted.replaceAll('mm', this.getMinutes().toString().padStart(2, '0'));
+    formatted = formatted.replaceAll('m', this.getMinutes().toString());
 
     return formatted;
   }
@@ -113,8 +113,8 @@ export class Datee extends Date {
   private formatSecond(format: string): string {
     let formatted = format;
 
-    formatted = formatted.replace('ss', this.getSeconds().toString().padStart(2, '0'));
-    formatted = formatted.replace('s', this.getSeconds().toString());
+    formatted = formatted.replaceAll('ss', this.getSeconds().toString().padStart(2, '0'));
+    formatted = formatted.replaceAll('s', this.getSeconds().toString());
 
     return formatted;
   }
@@ -122,8 +122,8 @@ export class Datee extends Date {
   private formatAMPM(format: string): string {
     const formatted = format;
 
-    // formatted = formatted.replace('A', this.getHours() < 12 ? 'AM' : 'PM');
-    // formatted = formatted.replace('a', this.getHours() < 12 ? 'am' : 'pm');
+    // formatted = formatted.replaceAll('A', this.getHours() < 12 ? 'AM' : 'PM');
+    // formatted = formatted.replaceAll('a', this.getHours() < 12 ? 'am' : 'pm');
 
     return formatted;
   }
@@ -131,9 +131,9 @@ export class Datee extends Date {
   private formatMillisecond(format: string): string {
     let formatted = format;
 
-    formatted = formatted.replace('SSS', this.getMilliseconds().toString().padStart(3, '0'));
-    formatted = formatted.replace('SS', this.getMilliseconds().toString().padStart(2, '0'));
-    formatted = formatted.replace('S', this.getMilliseconds().toString());
+    formatted = formatted.replaceAll('SSS', this.getMilliseconds().toString().padStart(3, '0'));
+    formatted = formatted.replaceAll('SS', this.getMilliseconds().toString().padStart(2, '0'));
+    formatted = formatted.replaceAll('S', this.getMilliseconds().toString());
 
     return formatted;
   }
@@ -141,8 +141,8 @@ export class Datee extends Date {
   private formatTimestamp(format: string): string {
     let formatted = format;
 
-    formatted = formatted.replace('X', this.getTime().toString());
-    formatted = formatted.replace('x', Math.floor(this.getTime() / 1000).toString());
+    formatted = formatted.replaceAll('X', this.getTime().toString());
+    formatted = formatted.replaceAll('x', Math.floor(this.getTime() / 1000).toString());
 
     return formatted;
   }
@@ -150,14 +150,14 @@ export class Datee extends Date {
   private formatTimezone(format: string): string {
     let formatted = format;
 
-    formatted = formatted.replace(
+    formatted = formatted.replaceAll(
       'ZZZZ',
       this.toTimeString()
         .match(/([+-]\d{2})(\d{2})/)
         ?.slice(1, 3)
         .join(':') ?? '',
     );
-    formatted = formatted.replace(
+    formatted = formatted.replaceAll(
       'ZZZ',
       this.toTimeString()
         .match(/[A-Z]{3}/)
